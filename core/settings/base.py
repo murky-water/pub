5d52a2a35b61a9d1af8ffbf117d55d14cd5525c1
henry <vtrquan94@gmail.com>
2025-01-23 10:26:21 +0700

    first

--- core/settings/base.py
+++ core/settings/base.py
@@ -78,6 +78,5 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = 'static/'
 # MEDIA_URL='media/'
-STATICFILES_DIRS = [
-os.path.join(BASE_DIR,'static/')]
-# STATIC_ROOT = os.path.join(Path(__file__).resolve().parent.parent.parent, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
